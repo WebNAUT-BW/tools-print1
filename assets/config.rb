f6b4ec6c9f4949c0af4951b43efd3ded5bdb6f47
Kotaro Shimoda <k_shimoda@beeworks.co.jp>
2014-11-27 21:05:05 +0900

    Bootstrap,個別CSS作業準備

---
+++ assets/config.rb
@@ -0,0 +1,27 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "./css"
+sass_dir = "./scss"
+images_dir = "./img"
+javascripts_dir = "./js"
+
+# output_style = :compressed
+environment = :development
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+# color_output = false
+relative_assets = true
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass shared/css/scss scss && rm -rf sass && mv scss sass
