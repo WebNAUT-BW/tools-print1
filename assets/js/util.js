9d4739f6720862c37924c725cfff2da4a8467bb8
Kotaro Shimoda <k_shimoda@beeworks.co.jp>
2014-12-25 13:48:17 +0900

    画像幅変更用のスライドコントローラー追加、列数設定の項目を削除、印刷ボタン追加、ソース整理

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,39 +1,96 @@
-var colNum      = 3;              //列数
 var itemWidth   = 320;            //画像幅
-var offsetAll   = 0;              //全体オフセット
-var offsetCol   = new Array(10);  //各列オフセット
 var paWidth     = 1000;           //印刷エリア幅
 var paHeight    = 1414;           //印刷エリア高さ
 var $_img       = $('img.disp');  //画像
 var $_pa        = $('#printArea');//印刷エリア
 var storage     = localStorage;
+var slider;
 
-// File API Dropイベント追加
 $(function () {
+	// File API Dropイベント追加
 	if (window.File) {
 		// File APIに関する処理を記述
 		document.getElementById("printArea").addEventListener("drop", onDrop, false);
 	} else {
 		window.alert("本ブラウザではFile APIが使えません");
 	}
+
+	//画像ドラッグ設定
+	$_img.draggable();
+
+	//スライダー関連
+	//値の初期化
+	var sliderValue = storage.getItem("print1-sliderValue");
+	if (!sliderValue){
+		sliderValue = 300;
+	}
+	//Initialize
+	slider = $( "#slider" ).slider({
+		max: 500,
+		min: 100,
+		value: sliderValue
+	});
+
+	//イベント
+	slider.on("slide",function(event,ui){
+		var value = ui.value;
+		$('img').css('width',value);
+		$('#inputItemWidth').val(value);
+	} );
+	slider.on("slidechange",function(event,ui){
+		var value = ui.value;
+		storage.setItem("print1-sliderValue", value);
+		storage.setItem("print1-itemWidth", value);
+	} );
 });
+
+//Angular.js関連
+var myApp = angular.module('print1', []);
+myApp.controller('print1Controller', ['$scope', function($scope){
+
+	//値の初期化
+	$scope.itemWidth = 320;
+	var getitemWidth = storage.getItem("print1-itemWidth");
+	if(getitemWidth){
+		$scope.itemWidth = getitemWidth;
+		itemWidth = getitemWidth;
+	}
+
+	//画像幅変更処理
+	$scope.changeItemWidth = function(num){
+		// console.log(num);
+		$('img').css('width',num);
+		itemWidth = num;
+		storage.setItem("print1-itemWidth",num);
+		slider.slider("option","value",num);//スライダーに値をセット
+	};
+
+	//クリア（リロード）
+	$scope.clear = function(){
+		window.location.reload();
+	};
+
+	//印刷
+	$scope.wPrint = function(){
+		window.print();
+	};
+}]);
+
 
 // Drop領域にドロップした際のファイルのプロパティ情報読み取り処理
 function onDrop(event) {
 	var files = event.dataTransfer.files;
 	$('#beforeMessage').text('loading...');
 	for (var i = 0; i < files.length; i++) {
-		var file = files[i],            // 2. files配列にファイルが入っています
-		fileReader = new FileReader();   // 4. ファイルを読み込むFileReaderオブジェクト
+		var file = files[i],
+		fileReader = new FileReader();
 
-		// 5. 読み込みが完了した際のイベントハンドラ。imgのsrcにデータをセット
 		fileReader.onload = function(event) {
 			// 読み込んだデータをimgに設定
 			$_img.attr('src', event.target.result);
 			$('#beforeMessage').hide();
 			imgSetInit();
 		};
-		// 6. 画像読み込み
 		fileReader.readAsDataURL(file);
 	}
 	event.preventDefault();
@@ -45,26 +102,22 @@ function imgSetInit() {
 	imgHeight = $_img.height();
 	var paWidth = $_pa.width(),
 	paHeight = $_pa.height();
-	// console.log('imgWidth=' + imgWidth + ', imgHeight=' + imgHeight);
 
 	if (paHeight > imgHeight) {
-		console.log('imgHeight under');
 		$_img.clone().addClass('clone').appendTo($('#printArea'));
 	} else {
-		// console.log('imgHeight over');
+		//画像コピー枚数計算用
 		var hval = (imgHeight / paHeight);
 		var hvalMath = Math.floor(imgHeight / paHeight);
-		// console.log('hval=' + hval);
-		// console.log('hvalMath=' + hvalMath);
 
 		//1枚目の画像幅指定
 		$_img.css('width',itemWidth);
-		for (var i = 0; i < (colNum-1); i++) {
+		for (var i = 0; i < hvalMath; i++) {
 			$_img.clone().addClass('clone').css(
 				{
 					//2枚目以降のスタイル設定
 					'left':(itemWidth*(i+1)),
-					'top':(1414*(i+1)*(-1)),
+					'top':(paHeight*(i+1)*(-1)),
 					'width':itemWidth
 				}
 			).appendTo($('#printArea')).draggable();;
@@ -74,71 +127,4 @@ function imgSetInit() {
 
 function onDragOver(event) {
 	event.preventDefault();
-}
-
-var myApp = angular.module('print1', []);
-myApp.controller('print1Controller', ['$scope', function($scope){
-
-	$scope.colNum = 3;
-	$scope.itemWidth = 320;
-
-	var getcolNum = storage.getItem("print1-colNum");
-	if(getcolNum){
-		$scope.colNum = getcolNum;
-		colNum = getcolNum;
-	}
-	var getitemWidth = storage.getItem("print1-itemWidth");
-	if(getitemWidth){
-		$scope.itemWidth = getitemWidth;
-		itemWidth = getitemWidth;
-	}
-
-	//列数変更処理
-	$scope.changeColNum = function(num){
-		// $scope.spice = spice;
-		$_pa.attr('data-colnum',num);
-		colNum = num;
-		storage.setItem("print1-colNum", num);
-	};
-
-	//画像幅変更処理
-	$scope.changeItemWidth = function(num){
-		// console.log(num);
-		$('img').css('width',num);
-		itemWidth = num;
-		storage.setItem("print1-itemWidth", num);
-	};
-
-	//全体オフセット変更処理
-	$scope.changeOffsetAll = function(num){
-		// $scope.spice = spice;
-		$_img.each(function(){
-			var _top = $(this).css('top');
-			$(this).css('top',_top+num);
-		});
-		// console.log(num);
-	};
-
-	//各列オフセット変更処理
-	$scope.changeOffsetCol = function(col,num){
-		// $scope.spice = spice;
-		// console.log(num);
-	};
-
-	//クリア
-	$scope.clear = function(){
-		clear();
-	};
-
-}]);
-
-$(function() {
-    $_img.draggable();
-});
-
-function clear() {
-	window.location.reload();
-	// $('#beforeMessage').show();
-	// $_pa.find('img').remove();
-	// $_pa.append('<img src="" alt="" class="disp">');
 }
